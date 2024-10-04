e70d79c1e09e8d798776ec5f01d490008690358c
Patrik Krizek <patrik.krizek@getsetuk.co.uk>
2024-10-04 13:24:02 +0100

    Week 8

---
+++ week-08/scripts/js-basics.js
@@ -0,0 +1,21 @@
+// Variables
+var animal = "dog";
+let firstName = "John";
+const fuelTank = 50;
+
+console.log(8 + 5);
+console.log("Joe" + " " + "Doe");
+
+function sumNumbers(x, y) {
+  return x + y;
+}
+
+console.log(sumNumbers(9, 20));
+
+// Create a function that will add two words together and display it on the console. (10min)
+
+function addWords(a, b) {
+  return a + " " + b;
+}
+
+console.log(addWords("Patrik", "Krizek"));
